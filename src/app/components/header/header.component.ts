bcd100738a3240f0ad8e9e70f3458ff5f9d69331
Debora <debora.roldanlara@gmail.com>
2025-01-14 01:08:57 -0300

    Update funcionamento de rotas

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,30 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
     // Lidar com o clique no menu hambúrguer
-    const hamburgerMenu = document.getElementById('hamburgerMenu') as HTMLElement;
+    const hamburgerMenu = document.getElementById(
+      'hamburgerMenu'
+    ) as HTMLElement;
     const sideMenu = document.getElementById('sideMenu') as HTMLElement;
     const closeMenu = document.getElementById('closeMenu') as HTMLElement;
 
     // Adicionar evento de clique ao menu hambúrguer
     hamburgerMenu.addEventListener('click', () => {
-      hamburgerMenu.classList.toggle('active');  // Alterna a classe 'active' para mudar o ícone do hambúrguer para 'X'
-      sideMenu.classList.toggle('show');  // Alterna a visibilidade do menu lateral
+      hamburgerMenu.classList.toggle('active'); // Alterna a classe 'active' para mudar o ícone do hambúrguer para 'X'
+      sideMenu.classList.toggle('show'); // Alterna a visibilidade do menu lateral
     });
 
     // Adicionar evento de clique ao botão de fechar
     closeMenu.addEventListener('click', () => {
-      sideMenu.classList.remove('show');  // Remove a classe 'show' para ocultar o menu
-      hamburgerMenu.classList.remove('active');  // Restaura o ícone do hambúrguer
+      sideMenu.classList.remove('show'); // Remove a classe 'show' para ocultar o menu
+      hamburgerMenu.classList.remove('active'); // Restaura o ícone do hambúrguer
     });
+  }
+
+  navigate() {
+    this.router.navigate(['dashboard']);
   }
 }
